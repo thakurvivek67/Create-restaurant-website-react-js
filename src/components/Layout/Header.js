314bb9b01f81320a71dbeeefcaf7e2fd52fc8509
thakurvivek67 <thakurvivek6767@gmail.com>
2024-05-17 18:26:13 +0530

    Add Restaurant Summary

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,19 @@
 import React, { Fragment } from "react";
-import mealsImage from "../../assests/meals.jpg";
+import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
-export default function Header() {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>cl
-        <button>Cart</button>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton></HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!"></img>
       </div>
     </Fragment>
   );
-}
+};
+export default Header;
