43a47332a617f5f690b8231cc1829b371a094f38
thakurvivek67 <thakurvivek6767@gmail.com>
2024-05-21 03:16:55 +0530

    Implementing the cart Itemns page

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/CartContext";
+
 
 const MealItem = (props) => {
+  const cartctx=useContext(CartContext)
   const price = `$${props.price.toFixed(2)}`;
+
+  const addCartToHandler=amount=>{
+    cartctx.addItem({
+      id:props.id,
+      name:props.name,
+      amount:amount,
+      price:props.price
+    }
+    )
+  }
   return (
     <li className={classes.meal}>
       <div>
@@ -12,7 +25,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={addCartToHandler}/>
       </div>
     </li>
   );
